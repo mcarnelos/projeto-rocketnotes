7a5fe03d51334aca6f359c76edc561314a50993a
Murilo Carnelos <muriloc.c@hotmail.com>
2022-12-14 22:32:33 -0300

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components' //importando ThemeProvider
 import theme from './styles/theme'
 import GlobalStyle from './styles/global'
 
-import { SignIn } from './pages/SignIn'
+import { Routes } from './routes'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <SignIn />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,18 +1,27 @@
 import { Container, Form, Background } from './styles'
-import { FiMail, FiLock } from 'react-icons/fi'
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
+
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
 
-export function SignIn() {
+export function SignUp() {
   return (
     <Container>
+      <Background />
+
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis.</p>
 
-        <h2>Faça seu login</h2>
+        <h2>Crie sua conta</h2>
+
+        <Input
+          placeholder='Nome'
+          type="text"
+          icon={FiUser}
+        />
 
         <Input
           placeholder='E-mail'
@@ -26,14 +35,13 @@ export function SignIn() {
           icon={FiLock}
         />
 
-        <Button title="Entrar" />
+        <Button title="Cadastrar" />
 
-        <Link to="/register">
-          Criar conta
+        <Link to="/">
+          Voltar para o login
         </Link>
       </Form>
 
-      <Background />
     </Container>
   )
 }
